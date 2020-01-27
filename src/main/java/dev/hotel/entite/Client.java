9cb559637e2c84579592f7f4bde20cae40881fd5
Roux JP <jproux343@gmail.com>
2020-01-27 17:04:49 +0100

    correctif 3

--- src/main/java/dev/hotel/entite/Client.java
+++ src/main/java/dev/hotel/entite/Client.java
@@ -2,14 +2,20 @@ package dev.hotel.entite;
 
 
 import javax.persistence.Entity;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 
-@Entity
+@Entity(name = "client")
 public class Client extends BaseEntite {
 
 	
+	@NotEmpty
+	@Size(min = 3, max = 50)
     private String nom;
 
+	@NotEmpty
+	@Size(min = 4)
     private String prenoms;
 
     public Client() {
